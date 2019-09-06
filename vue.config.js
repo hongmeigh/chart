c773d0572da019c387242012329238abce135980
hongmei.gao <hongmei168_gao@163.com>
2019-09-06 12:31:27 +0800

    md

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    publicPath: './',
+    // chainWebpack: config => {
+    //   config.module
+    //     .rule('json')
+    //     .test(/\.json$/)
+    //     .use('json-loader')
+    //       .loader('json-loader')
+    //       .tap(options => {
+    //         // 修改它的选项...
+    //         return options
+    //       })
+    // }
+}
